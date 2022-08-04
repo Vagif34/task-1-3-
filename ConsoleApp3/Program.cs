71b6598623ef58e3f59a5cb4c151da0e73673724
Vagif34 <vagifrg@code.edu.az>
2022-08-04 18:41:27 +0400

    new file

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine();
+           //int Min =int.Parse(Console.ReadLine());
+
+            
+        }
+        static int Min(int[] arr )
+        {
+            //int[] arr = { 4, 3, 2, 5, 6 };
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                {
+                    if (min < arr[0])
+
+                    
+                        min = arr[i];
+                    
+                }
+            }
+            return min;
+            Console.WriteLine(min);
+          
+
+
+
+
+
+        }
+    }
+}
